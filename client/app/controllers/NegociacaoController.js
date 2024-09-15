b41b0b8c1a8e2a1e6222b304eb924d4534e0baf3
Paulo Felipe Souza <alparakleto@gmail.com>
2024-09-15 10:23:07 -0300

    show date, quantity and value as strings
    we refactor variables and id nodes to be in English. I found it hard to
    read it in Portuguese.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -4,5 +4,12 @@ class NegociacaoController {
     event.preventDefault();
 
     alert('Chamei no controller')
+
+    let inputDate = document.querySelector('#date');
+    let inputQuantity = document.querySelector('#quantity');
+    let inputValue = document.querySelector('#value');
+    console.log(inputDate.value);
+    console.log(inputQuantity.value);
+    console.log(inputValue.value);
   }
 }
