08c95333685ffe23c34a7f4f1dbfa96913a7e7f1
Paulo Felipe Souza <alparakleto@gmail.com>
2024-10-01 11:36:52 -0300

    create private method _createNegotiation
    to show up negotiation creation even more.

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -12,27 +12,24 @@ class NegociacaoController {
     // cancela a submissao do formulario
     event.preventDefault();
 
-    alert('Chamei no controller')
-
     let converter = DateConverter;
 
     // I mention to JQuery, we will bind the $ variable to the document context
     let date = converter.toDate(this._inputDate.value);
 
-    let tradeNegociation = new Negociacao(
-      date,
-      this._inputQuantity.valueAsNumber,
-      parseFloat(this._inputValue.value)
-    );
+    this._negotiations.add(this._createNegotiation());
 
-    this._negotiations.add(tradeNegociation);
-
-    let dayMonthYear = converter.toText(tradeNegociation.date)
-    console.log(dayMonthYear);
-    console.log(tradeNegociation);
     console.log(this._negotiations.toArray());
 
     this._cleanForm();
+  }
+
+  _createNegotiation(){
+    return new Negociacao(
+      DateConverter.toDate(this._inputDate.value),
+      this._inputQuantity.valueAsNumber,
+      parseFloat(this._inputValue.value)
+    );
   }
 
   _cleanForm() {
