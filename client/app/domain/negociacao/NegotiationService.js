cbfda780cd72298abeb92f655f8eac1d75f1d4d6
Paulo Felipe Souza <alparakleto@gmail.com>
2024-10-28 23:46:55 -0300

    Change NegotiationService to Promisse pattern
    The Promisse pattern receives in its constructor two functions
    callbacks, the first one is the resolve and the second one is the reject
    result.

--- client/app/domain/negociacao/NegotiationService.js
+++ client/app/domain/negociacao/NegotiationService.js
@@ -1,23 +1,25 @@
 class NegotiationService {
   weekly(callback){
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'negociacoes/semana');
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState == 4) {
-        if (xhr.status == 200) {
-          console.log('receiving negotiations from server');
-          console.log(JSON.parse(xhr.responseText));
-          const negotiations = JSON.parse(xhr.responseText).map( item => new Negociacao(new Date(item.data), item.quantidade, item.valor))
-     //     .forEach(negotiation => this._negotiations.add(negotiation));
-          callback(null, negotiations)
-        } else {
-            console.log(xhr.responseText);
-       //     this._message.text = 'It was not possible to get the weekly negotiations';
-          callback('It was not possible to get the weekly negotiations',  null);
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'negociacoes/semana');
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            console.log('receiving negotiations from server');
+            console.log(JSON.parse(xhr.responseText));
+            const negotiations = JSON.parse(xhr.responseText).map( item => new Negociacao(new Date(item.data), item.quantidade, item.valor))
+      //     .forEach(negotiation => this._negotiations.add(negotiation));
+           resolve(negotiations)
+          } else {
+              console.log(xhr.responseText);
+        //     this._message.text = 'It was not possible to get the weekly negotiations';
+            reject('It was not possible to get the weekly negotiations',  null);
+          }
         }
       }
-    }
 
-    xhr.send();
+      xhr.send();
+      })
   }
 }
