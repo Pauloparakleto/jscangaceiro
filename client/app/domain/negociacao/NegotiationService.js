001bf7a2d5d9a79e269e5a058130990bf0ae2aef
Paulo Felipe Souza <alparakleto@gmail.com>
2024-10-29 11:55:00 -0300

    13.4 separate http service and negotiation service responsabilities
    Http service handle the promisse success and rejection.
    The Negotiation service either create negotiation based on data or
    handle the error.

--- client/app/domain/negociacao/NegotiationService.js
+++ client/app/domain/negociacao/NegotiationService.js
@@ -1,25 +1,24 @@
 class NegotiationService {
-  weekly(callback){
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', 'negociacoes/semana');
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-            console.log('receiving negotiations from server');
-            console.log(JSON.parse(xhr.responseText));
-            const negotiations = JSON.parse(xhr.responseText).map( item => new Negociacao(new Date(item.data), item.quantidade, item.valor))
-      //     .forEach(negotiation => this._negotiations.add(negotiation));
-           resolve(negotiations)
-          } else {
-              console.log(xhr.responseText);
-        //     this._message.text = 'It was not possible to get the weekly negotiations';
-            reject('It was not possible to get the weekly negotiations',  null);
-          }
-        }
-      }
+  constructor(){
+    this._http = new HTTPService();
+  }
 
-      xhr.send();
-      })
+  weekly(callback){
+    return this._http.get('negociacoes/semana')
+      .then(
+        data =>
+          {
+            const negotiations = data.map(
+              negotiation => new Negociacao(new Date(negotiation.data), negotiation.quantidade, negotiation.valor)
+            );
+
+            return negotiations;
+          },
+
+        error => 
+          {
+            throw new Error('Something went wrong importing weekly negotiations')
+          }
+      )
   }
 }
