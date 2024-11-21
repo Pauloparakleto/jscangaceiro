b7be9a2d363ae814a3aae9c36e08c050a2cdbac0
Paulo Felipe Souza <alparakleto@gmail.com>
2024-11-20 21:15:45 -0300

    NegotiationDao draft

---
+++ client/app/domain/negociacao/NegotiationDao.js
@@ -0,0 +1,18 @@
+class NegotiationDao {
+  constructor (connection){
+    this._connection = connection;
+    this._store = 'negotiation';
+  }
+
+  add(negotiation){
+    return new Promisse((resolve, reject) => {
+
+    });
+  }
+
+  listAll(){
+    return new Promisse((resolve, reject) => {
+
+    });
+  }
+}
