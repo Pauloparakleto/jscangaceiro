f9a33b28f27f8645d0468a0a26f7f57163dc2043
Paulo Felipe Souza <alparakleto@gmail.com>
2024-11-20 21:37:13 -0300

    Persist negotiations through NegotiationDao
    This is the Data Access Object Pattern.
    Check also the Active Record Pattern from the Ruby on Rails convention
    to dive into the similarity and difference between them.

--- client/app/domain/negociacao/NegotiationDao.js
+++ client/app/domain/negociacao/NegotiationDao.js
@@ -1,12 +1,20 @@
 class NegotiationDao {
   constructor (connection){
     this._connection = connection;
-    this._store = 'negotiation';
+    this._store = 'negotiations';
   }
 
   add(negotiation){
-    return new Promisse((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+      const request = this._connection
+            .transaction([this._store], 'readwrite')
+            .objectStore(this._store).add(negotiation);
 
+      request.onsuccess = e => resolve();
+      request.onerror = e => {
+        console.log(e.target.error);
+        reject('It was not possible to save this negotiation');
+      };
     });
   }
 
