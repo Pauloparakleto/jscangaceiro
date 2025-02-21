3c7fff9ea9e4163633fb63ea97259991c5f1931e
Paulo Felipe Souza <alparakleto@gmail.com>
2025-02-20 22:33:41 -0300

    remove debounce call in app.js
    Debounce will be a decorator pattern inside the controller.

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,12 +1,9 @@
 import { NegociacaoController } from "./controllers/NegociacaoController.js";
-import { debounce } from "./util/Debounce.js";
+import { debounce } from "./util/index.js";
 
 const controller = new NegociacaoController();
 const $ = document.querySelector.bind(document);
 
 $('.form').addEventListener("submit", controller.adiciona.bind(controller));
 $('#botao-apaga').addEventListener("click", controller.clearIndex.bind(controller));
-$('#botao-importa')
-  .addEventListener("click", debounce(() => {
-    controller.importIndex();
-  }, 1000));
+$('#botao-importa').addEventListener("click", controller.importIndex.bind(controller));
