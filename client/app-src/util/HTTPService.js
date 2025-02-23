6537d7e31828330cd085808ff758f6c63c3e4965
Paulo Felipe Souza <alparakleto@gmail.com>
2025-02-23 15:57:17 -0300

    refactor httpservice
    Use fetch function hencefoward.

--- client/app-src/util/HTTPService.js
+++ client/app-src/util/HTTPService.js
@@ -1,19 +1,12 @@
 export class HTTPService {
-  get(url){
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', url);
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-            resolve(JSON.parse(xhr.responseText));
-          } else {
-            reject('It was not possible to get the resource');
-          }
-        }
-      }
+  _handleErrors(response){
+    if(!response.ok) throw new Error(response.statusText);
+    return response;
+  }
 
-      xhr.send();
-    })
+  get(url){
+    return fetch(url)
+      .then(response => this._handleErrors(response)
+      .then(response => response.json());
   }
 }
