2927768a34a1d707e9c7a2c0ec917a0e376413cd
Paulo Felipe Souza <alparakleto@gmail.com>
2025-02-26 22:33:55 -0300

    provide bind metadata to controller
    the action containing the metadata will bind the document selector to
    the corresponding controller action.

--- client/app-src/util/Decorators/Controller.js
+++ client/app-src/util/Decorators/Controller.js
@@ -6,10 +6,29 @@ export function controller(...selectors) {
     const targetConstructor = constructor;
     const prettyConstructor = function() {
       return new targetConstructor(...elements);
-    }
+    };
+
+    const instance = new targetConstructor(...elements);
+
+    Object
+      .getOwnPropertyNames(targetConstructor.prototype)
+      .forEach(property => {
+        if (Reflect.hasMetadata('bindEvent', instance, property)) {
+          _bindEvent(instance,
+            Reflect.getMetadata('bindEvent', instance, property));
+        }
+      });
 
     prettyConstructor.prototype = targetConstructor.prototype;
 
     return prettyConstructor;
+  }
+
+  function _bindEvent(instance, metadata) {
+    document.querySelector(metadata.selector)
+    .addEventListener(metadata.event, event => {
+        if(metadata.preventDefault) event.preventDefault();
+      instance[metadata.propertyKey](event);
+      });
   }
 }
