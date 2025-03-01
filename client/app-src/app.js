39291f3d728449d979ab6f4b31428f6f4c427da8
Paulo Felipe Souza <alparakleto@gmail.com>
2025-03-01 20:53:35 -0300

    Load bootstrap css theme using webpack plugins loaders

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import '../app-src/css/custom.css';
 import { NegociacaoController } from "./controllers/NegociacaoController.js";
 import { debounce } from "./util/index.js";
 
