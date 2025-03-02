548a33cd1d5c0ba4dacc08a93e58651eefb4d065
Paulo Felipe Souza <alparakleto@gmail.com>
2025-03-01 21:14:30 -0300

    remove custom css sample

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
-import '../app-src/css/custom.css';
 import { NegociacaoController } from "./controllers/NegociacaoController.js";
 import { debounce } from "./util/index.js";
 
