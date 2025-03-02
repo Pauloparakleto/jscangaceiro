93cf36a94201ce96a66860498ec794b0fe5e2293
Paulo Felipe Souza <alparakleto@gmail.com>
2025-03-02 20:44:04 -0300

    optimize css for production with plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
+
 const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
+const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 let plugins = [];
 
 plugins.push(
@@ -10,6 +13,15 @@ plugins.push(
 
 if (process.env.NODE_ENV == 'production') {
   plugins.push(new babiliPlugin());
+  plugins.push(new optimizeCSSAssetsPlugin({
+    cssProcessor: require('cssnano'),
+    cssProcessorOptions: {
+      discardComments: {
+        removeAll: true
+      }
+    },
+    canPrint: true
+  }));
 }
 
 module.exports = {
